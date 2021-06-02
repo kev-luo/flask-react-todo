5fe0d4e52683410f93041a92990636b7070c07f9
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-02 19:46:52 -0400

    set up db

--- api/api.py
+++ api/api.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////{}".format(os.path.join(basedir, "example.db"))
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+    def __str__(self):
+        return f"{self.id} {self.content}"
+
+
+def todo_serializer(todo):
+    return {"id": todo.id, "content": todo.content}
 
 
 @app.route("/api", methods=["GET"])
 def index():
-    return {"name": "Hello World"}
+    # unpack the map into an array using '*'
+    return jsonify([*map(todo_serializer, Todo.query.all())])
 
 
 if __name__ == "__main__":
