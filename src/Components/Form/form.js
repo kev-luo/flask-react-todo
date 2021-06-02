4b4a5a23f2f2ae91c118ff469030f79b40f92165
Kevin Luo <kevinluo@Kevins-MacBook-Air.local>
2021-06-02 19:52:13 -0400

    add form component

---
+++ src/Components/Form/form.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <>
+      <form>
+        <input type="text" required></input>
+        <input type="submit"></input>
+      </form>
+    </>
+  );
+}
--- src/Pages/TodoPage.js
+++ src/Pages/TodoPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Card from "../Components/Card/card";
+import Form from "../Components/Form/form";
 
 export default function TodoPage() {
   const [todo, setTodo] = useState([]);
@@ -14,6 +15,7 @@ export default function TodoPage() {
   }, []);
   return (
     <>
+      <Form />
       <Card listOfTodos={todo} />
     </>
   );
